ccc20cff7188d168f35ced31b8db60d72d3bdecc
michalantolik <michal.antolik@gmail.com>
2026-01-28 22:19:12 +0100

    Add CEP for Internal Server Error (500)

---
+++ MvcMovie/MvcMovie/Controllers/CEPTestingController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class CEPTestingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region Force specific HttpStatus
+
+        [HttpPost]
+        public ActionResult HttpStatus500()
+        {
+            return new HttpStatusCodeResult(500, "Forced HttpStatus 500");
+        }
+
+        #endregion Force specific HttpStatus
+
+
+        #region Force specific HttpException
+
+        [HttpPost]
+        public ActionResult HttpException500()
+        {
+            throw new HttpException((int)HttpStatusCode.InternalServerError, "Forced HttpException 500");
+        }
+
+        #endregion Force specific HttpException
+
+
+        #region Force other C# excepction
+
+        [HttpPost]
+        public ActionResult NullReferenceException()
+        {
+            throw new NullReferenceException();
+        }
+
+        [HttpPost]
+        public ActionResult InvalidOperationException()
+        {
+            throw new InvalidOperationException();
+        }
+
+        [HttpPost]
+        public ActionResult DivideByZeroException()
+        {
+            throw new DivideByZeroException();
+        }
+
+        #endregion Force other C# excepction
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    [HandleError]
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// Show Fallback CEP.
+        /// </summary>
+        public ActionResult FallbackError()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Show CEP for "InternalServerError, 500".
+        /// </summary>
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+    }
+}
