4203fe36e6a2b1ef280e205df92bb2af433396f2
michalantolik <michal.antolik@gmail.com>
2026-01-28 14:09:37 +0100

    Add exception buttons

---
+++ MvcMovie/MvcMovie/Controllers/ErrorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [HttpPost]
+        public ActionResult NullReferenceException()
+        {
+            throw new NullReferenceException();
+        }
+
+        [HttpPost]
+        public ActionResult InvalidOperationException()
+        {
+            throw new InvalidOperationException();
+        }
+
+        [HttpPost]
+        public ActionResult DivideByZeroException()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
